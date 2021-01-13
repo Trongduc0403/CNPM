875e94a6e031fcd25b76372714c69996afb8d63a
Trongduc0403 <trongduc0403@gmail.com>
2021-01-13 09:52:53 +0700

    update csdl

---
+++ QuanLyChoThueOto/QuanLyChoThueOto/Controller/HopDongController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyChoThueOto.Models;
+
+namespace QuanLyChoThueOto.Controller
+{
+    class HopDongController
+    {
+        public static bool AddHopDong(HopDong hopdong)
+        {
+            using (var _context = new CNPMEntities())
+            {
+                _context.HopDongs.Add(hopdong);
+                _context.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
